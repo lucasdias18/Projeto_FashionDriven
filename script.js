7eb07c8d1d4513ccc9b4044b21cc9a2a8f90d91d
Lucas da Silva Dias <lucasdias.ld10@gmail.com>
2022-11-10 17:04:57 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const nome = prompt('Qual é o seu nome?')
 
+
 function carregarSite() {
     const pedidos_anteriores = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts")
     pedidos_anteriores.then(carregarCamisas)
     pedidos_anteriores.catch(Erro)
 
     function carregarCamisas(resposta) {
-        // console.log(resposta)
         const pedidos_camisas = document.querySelector('footer div')
         pedidos_camisas.innerHTML = ``
         for (i = 0; i < 10; i++) {
             pedidos_camisas.innerHTML += `<figure onclick='encomendaIdentica(this)'>
                                             <img src="${resposta.data[i].image}" />
-                                            <figcaption><b>Criador: </b>${resposta.data[i].owner}</figcaption>
+                                            <figcaption><b>Criador: </b><span>${resposta.data[i].owner}</span></figcaption>
+                                            <div class="dados">${resposta.data[i].material}</div>
+                                            <div class="dados">${resposta.data[i].neck}</div>
+                                            <div class="dados">${resposta.data[i].model}</div>
                                         </figure>`
         }
+
     }
 
     function Erro(erro) {
@@ -22,13 +26,32 @@ function carregarSite() {
     }
 }
 
-setInterval(carregarSite,1000)
+
+setInterval(carregarSite,500)
+
 
 function encomendaIdentica(camisa) {
-    // confirm("Deseja fazer uma encomenda idêntica à camisa selecionada?")
+    const image = camisa.children[0].getAttribute('src')
+    const owner = nome
+    const material = camisa.children[2].innerHTML
+    const neck = camisa.children[3].innerHTML
+    const model = camisa.children[4].innerHTML
+    const author = nome
+
+    const encomenda_identica = {"model": model, "neck": neck, "material": material, "image": image, "owner": owner, "author": author}
 
     if (confirm("Deseja fazer uma encomenda idêntica à camisa selecionada?") === true ) {
-        alert('Ok!')
+        const enviar_pedido = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", encomenda_identica)
+        enviar_pedido.then(pedidoRealizado)
+        enviar_pedido.catch(falhaPedido)
+
+        function pedidoRealizado(resposta) {
+            alert("Encomenda realizada.")
+        }
+
+        function falhaPedido(erro) {
+            alert("Ops, não conseguimos processar sua encomenda.")
+        }
     }
 
     else {
@@ -36,10 +59,6 @@ function encomendaIdentica(camisa) {
     }
 }
 
- function Erro(erro) {
-    console.log("Status code: " + erro.response.status);
-    console.log("Mensagem de erro: " + erro.response.data);
-  }
 
 function selecionarModelo(modelo) {
     let elementos = document.querySelectorAll('.modelo');
@@ -49,6 +68,7 @@ function selecionarModelo(modelo) {
     modelo.classList.add('selecionado')
 }
 
+
 function selecionarGola(gola) {
     let elementos = document.querySelectorAll('.gola');
     for (i=0; i < elementos.length; i++) {
@@ -56,6 +76,7 @@ function selecionarGola(gola) {
     }
     gola.classList.add('selecionado')
 }
+
 
 function selecionarTecido(tecido) {
     let elementos = document.querySelectorAll('.tecido');
@@ -65,82 +86,80 @@ function selecionarTecido(tecido) {
     tecido.classList.add('selecionado')
 }
 
+
 function ativarBotao() {
     let link = document.querySelector('input').value
     let selecionados = document.querySelectorAll('.selecionado')
 
     if (selecionados.length === 3 && link !== "") {
         const botao = document.querySelector('.finalizar-pedido')
-        botao.children[1].innerHTML = `<button class='ativado' onclick="confirmarPedido()">Confirmar pedido</button>`
+        botao.children[1].classList.add('ativado')
     }
 }
+
 
 setInterval (ativarBotao, 1000)
 
 
 function confirmarPedido() {
     let selecionados = document.querySelectorAll('.selecionado')
-
     let model, neck, material;
+    let botao = document.querySelector('.finalizar-pedido > button')
 
-    if (selecionados[0].parentNode.children[1] === 'T-Shirt') {
-        model = 't-shirt'
+    if (botao.classList.contains('ativado') === true) {
+
+        if (selecionados[0].parentNode.children[1] === 'T-Shirt') {
+            model = 't-shirt'
+        }
+
+        else if (selecionados[0].parentNode.children[1] === 'Camiseta') {
+            model = 'top-tank'
+        }
+
+        else {
+            model = 'long'
+        }
+
+        if (selecionados[1].parentNode.children[1] === 'Gola V') {
+            neck = 'v-neck'
+        }
+
+        else if (selecionados[1].parentNode.children[1] === 'Gola Redonda') {
+            neck = 'round'
+        }
+
+        else {
+            neck = 'polo'
+        }
+
+        if (selecionados[2].parentNode.children[1] === 'Seda') {
+            material = 'silk'
+        }
+
+        else if (selecionados[2].parentNode.children[1] === 'Algodão') {
+            material = 'cotton'
+        }
+
+        else {
+            material = 'polyester'
+        }
+
+        const image = document.querySelector('input').value
+        const owner = nome
+        const author = nome
+
+        const dados = {"model": model, "neck": neck, "material": material, "image": image, "owner": owner, "author": author}
+
+        const enviar_pedido = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", dados)
+        enviar_pedido.then(pedidoRealizado)
+        enviar_pedido.catch(falhaPedido)
+
+        function pedidoRealizado(resposta) {
+            alert("Encomenda realizada.")
+        }
+
+        function falhaPedido(erro) {
+            alert("Ops, não conseguimos processar sua encomenda.")
+        }
     }
-
-    else if (selecionados[0].parentNode.children[1] === 'Camiseta') {
-        model = 'top-tank'
-    }
-
-    else {
-        model = 'long'
-    }
-
-    if (selecionados[1].parentNode.children[1] === 'Gola V') {
-        neck = 'v-neck'
-    }
-
-    else if (selecionados[1].parentNode.children[1] === 'Gola Redonda') {
-        neck = 'round'
-    }
-
-    else {
-        neck = 'polo'
-    }
-
-    if (selecionados[2].parentNode.children[1] === 'Seda') {
-        material = 'silk'
-    }
-
-    else if (selecionados[2].parentNode.children[1] === 'Algodão') {
-        material = 'cotton'
-    }
-
-    else {
-        material = 'polyester'
-    }
-
-    const image = document.querySelector('input').value
-    console.log(typeof(image))
-    const owner = nome
-    console.log(typeof(owner))
-    const author = nome
-
-    const dados = {"model": model, "neck": neck, "material": material, "image": image, "owner": owner, "author": author}
-    console.log(dados)
-
-    const enviar_pedido = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", dados)
-    enviar_pedido.then(pedidoRealizado)
-    enviar_pedido.catch(falhaPedido)
-
-    function pedidoRealizado(resposta) {
-        const statusResposta = resposta.status;
-	    console.log(statusResposta);
-    }
-
-    function falhaPedido(erro) {
-        console.log("Status code: " + erro.response.status);
-        console.log("Mensagem de erro: " + erro.response.data);
-        alert("Ops, não conseguimos processar sua encomenda")
-    }
-    
 }
